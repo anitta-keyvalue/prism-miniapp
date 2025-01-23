b11e47a2eac97034840eab208c64d97f80949ea0
Benil <benil@keyvalue.systems>
2025-01-23 15:27:45 +0530

    Feat/mop settings UI (#3)
    * fix: revert
    
    * fix: revert
    
    * feat: move SwitchBox to components
    
    * feat: create MopSettings UI and move CarpetSettings to settings folder
    
    ---------
    
    Co-authored-by: Benil <benil@lm-kv-benil-jose.local>

--- miniapp/src/pages/setting/cleaningSettings/carpetSettings/index.config.ts
+++ miniapp/src/pages/setting/cleaningSettings/carpetSettings/index.config.ts
@@ -1,4 +1,4 @@
 export default {
-  navigationBarTitleText: 'Home',
-  disableScroll: true,
+  navigationBarTitleText: "Home",
+  disableScroll: true
 };
--- miniapp/src/pages/setting/cleaningSettings/index.tsx
+++ miniapp/src/pages/setting/cleaningSettings/index.tsx
@@ -1,34 +1,36 @@
-import React, { FC } from 'react';
-import { Cell, CellGroup } from '@ray-js/smart-ui';
-import { router } from '@ray-js/ray';
+import React, { FC } from "react";
+import { CellGroup } from "@ray-js/smart-ui";
+import { router } from "@ray-js/ray";
 
-import { support } from '@/devices';
-import { carpetCleanPreferCode, mopExtendFrequencyCode } from '@/constant/dpCodes';
-import Strings from '@/i18n';
+import { support } from "@/devices";
+import {
+  carpetCleanPreferCode,
+  mopExtendFrequencyCode
+} from "@/constant/dpCodes";
+import Strings from "@/i18n";
+
+import SettingsCell from "../settingsCell";
 
 const CleaningSettings: FC = () => {
   return (
     <CellGroup>
       {support.isSupportDp(carpetCleanPreferCode) && (
-        <Cell
-          title={Strings.getLang('dsc_carpet_settings')}
-          isLink
+        <SettingsCell
+          title={Strings.getLang("dsc_carpet_settings")}
           onClick={() => {
-            router.push('/carpetSettings');
+            router.push("/carpetSettings");
           }}
         />
       )}
       {support.isSupportDp(mopExtendFrequencyCode) && (
-        <Cell
-          title={Strings.getLang('dsc_mop_settings')}
-          isLink
+        <SettingsCell
+          title={Strings.getLang("dsc_mop_settings")}
           onClick={() => {
-            router.push('/mopSettings');
+            router.push("/mopSettings");
           }}
         />
       )}
     </CellGroup>
   );
 };
-
 export default CleaningSettings;
